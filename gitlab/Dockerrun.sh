0d76d68074934e0817e1a5741e1929609a8fdbfe
Schmidtty <steven.schmidt@snyk.io>
2022-08-10 19:46:39 -0400

    Update Dockerrun.sh

--- gitlab/Dockerrun.sh
+++ gitlab/Dockerrun.sh
@@ -6,7 +6,7 @@ docker run -d \
 --name="gitlab-broker" \
 --restart=always \
 --network SnykBrokerNetwork \
--p 8000:8000 \
+-p 8003:8003 \
 -v ~/Workspace/snyk_broker/gitlab:/private \
 -e BROKER_TOKEN=$GITLAB_BROKER_TOKEN \
 -e LOG_LEVEL=info \
@@ -15,7 +15,7 @@ docker run -d \
 -e BITBUCKET_PASSWORD=$BITBUCKET_PASSWORD \
 -e BITBUCKET=bitbucket.797enterprises.com:7990 \
 -e BITBUCKET_API=bitbucket.797enterprises.com:7990/rest/api/1.0 \
--e BROKER_CLIENT_URL=http://broker.797enterprises.com:8000 \
--e PORT=8000 \
--e GIT_CLIENT_URL=http://code-agent:3000 \
+-e BROKER_CLIENT_URL=http://broker.797enterprises.com:8003 \
+-e PORT=8003 \
+-e GIT_CLIENT_URL=http://code-agent:3001 \
 snyk/broker:gitlab
